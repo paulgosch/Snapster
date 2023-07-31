a188a622f26bd2619e418238b09ea651d2ccec98
rintt <ufukalgac@github.com>
2023-07-31 22:26:56 +0200

    added constants file

---
+++ constants.js
@@ -0,0 +1,9 @@
+export const Pages = {
+    HomeScreen:'HomeScreen', 
+    WelcomePage:'WelcomePage', 
+    LoginPage: 'LoginPage', 
+    RegisterPage:'RegisterPage', 
+    AppPresentation:'AppPresentation', 
+    AppPresentation2:'AppPresentation2', 
+    AppPresentation3:'AppPresentation3'
+};
