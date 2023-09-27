21754c299d1e98cbae1006f3cdb57b2bcc93d2c7
Paul Gosch <140626536+paulgosch@users.noreply.github.com>
2023-09-27 11:34:58 +0200

    connected to playstore and applestore

--- SettingsPage.js
+++ SettingsPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Switch, StyleSheet, ImageBackground, Linking } from 'react-native';
+import { View, Text, TouchableOpacity, Switch, StyleSheet, ImageBackground, Linking,Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Pages, Colors, Fonts } from './constants';
 import FeatherIcon from 'react-native-vector-icons/Feather';
@@ -7,6 +7,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 
 const backgroundImageSource = require('./assets/Background.jpg');
+const appleStoreURL = "https://apps.apple.com/us/app/facebook/id284882215"; // Example link for the Facebook app on the App Store
+const googlePlayURL = "https://play.google.com/store/apps/details?id=com.facebook.katana"; // Example link for the Facebook app on Google Play
+
 
 export default function SettingsPage() {
   const [soundEffectsEnabled, setSoundEffectsEnabled] = useState(false);
@@ -41,8 +44,27 @@ export default function SettingsPage() {
   const handleBack = () => {
     navigation.goBack();
   };
+  
   const handleRateApp = () => {
-    // Implement the function to navigate to the app store page
+    Alert.alert(
+      "Rate this App",
+      "Choose a store to rate the app:",
+      [
+        {
+          text: "🍏 Apple App Store",
+          onPress: () => Linking.openURL(appleStoreURL).catch(err => console.error("Couldn't load App Store page", err))
+        },
+        {
+          text: "▶️ Google Play Store",
+          onPress: () => Linking.openURL(googlePlayURL).catch(err => console.error("Couldn't load Google Play Store page", err))
+        },
+        {
+          text: "Cancel",
+          style: "cancel"
+        }
+      ],
+      { cancelable: true }
+    );
   };
 
 
