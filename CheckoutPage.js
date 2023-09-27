64cecd2e7940c41bb25567792d055733d9eca6bd
Paul Gosch <140626536+paulgosch@users.noreply.github.com>
2023-09-27 12:08:07 +0200

    not finished

--- CheckoutPage.js
+++ CheckoutPage.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert, TouchableOpacity, ImageBackground, TextInput } from 'react-native'; // Import ImageBackground
+import { View, Text, StyleSheet, Button, Alert, TouchableOpacity, ImageBackground, TextInput,Image } from 'react-native'; // Import ImageBackground
 import axios from 'axios';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native'; // Import the useNavigation hook from React Navigation
-import { Pages,Colors } from './constants';
+import { Pages,Colors, Fonts } from './constants';
 import { useStripe, CardField, useConfirmPayment } from '@stripe/stripe-react-native';
 import Constants from "expo-constants";
 
@@ -12,7 +12,10 @@ const YOUR_CLIENT_ID = 'AZC-5Z6zhxR338bOISEchhRL13JfwG-JCGCmgsJPBpZzntabmjEsd9Ki
 
 // Import the backgroundImageSource from StorePage
 const backgroundImageSource = require('./assets/Background.jpg');
+const paypalimage = require('./assets/paypalimage.png'); // Import the PayPal image
 const API_URL = "http://172.16.62.62:3000";
+const cardImage = require('./assets/CardPaymentMasterCard.png');
+
 const CheckoutPage = () => {
   const stripe = useStripe();
   const [cardDetails, setCardDetails] = useState();
@@ -96,35 +99,55 @@ const CheckoutPage = () => {
   const handleGoBack = () => {
     navigation.goBack(); // Navigate back to the previous screen
   };
+   const handlePaypalCheckout = () => {
+    // Handle the PayPal checkout logic here
+  };
 
 
   return (
-    <View style={styles.container}>
-              <TouchableOpacity style={styles.backButton} onPress={handleGoBack}>
+    <ImageBackground source={backgroundImageSource} style={styles.backgroundContainer}>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.backButton} onPress={handleGoBack}>
           <Icon name="arrow-left" size={24} color="white" />
         </TouchableOpacity>
-        <Button title="Paypal" onPress={createOrder} />
-    <TextInput
-      autoCapitalize="none"
-      placeholder="E-mail"
-      keyboardType="email-address"
-      onChange={value => setEmail(value.nativeEvent.text)}
-      style={styles.input}
-    />
-    <CardField
-      postalCodeEnabled={true}
-      placeholder={{
-        number: "4242 4242 4242 4242",
-      }}
-      cardStyle={styles.card}
-      style={styles.cardContainer}
-      onCardChange={cardDetails => {
-        setCardDetails(cardDetails);
-      }}
-    />
-    <Button onPress={capturePayment} title="Pay with Card" disabled={loading} />
-  </View>
-);
+        <Text style={styles.title}>Payment Method</Text>
+        <View style={styles.cardImageContainer}>
+        <Image source={cardImage} style={styles.cardImage} resizeMode="contain" />
+      </View>
+        <TextInput
+          autoCapitalize="none"
+          placeholder="E-mail"
+          keyboardType="email-address"
+          onChange={value => setEmail(value.nativeEvent.text)}
+          style={styles.input}
+        />
+        <CardField
+          postalCodeEnabled={false}
+          placeholder={{
+            number: "4242 4242 4242 4242",
+          }}
+          cardStyle={styles.card}
+          style={styles.cardContainer}
+          onCardChange={cardDetails => {
+            setCardDetails(cardDetails);
+          }}
+        />
+          <TouchableOpacity 
+        onPress={capturePayment} 
+        style={styles.payButtonContainer} 
+        disabled={loading}
+      >
+        <Text style={styles.payButtonText}>Pay with Card</Text>
+      </TouchableOpacity>
+
+        <Text style={styles.cardManagementTitle}>Or Check out with</Text>
+
+      <TouchableOpacity onPress={createOrder} style={styles.paypalImageContainer}>
+        <Image source={paypalimage} style={styles.paypalImage} resizeMode="contain" />
+      </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
 };
 const styles = StyleSheet.create({
   container: {
@@ -134,7 +157,6 @@ const styles = StyleSheet.create({
   },
   input: {
     backgroundColor: "#efefefef",
-
     borderRadius: 8,
     fontSize: 20,
     height: 50,
@@ -146,6 +168,61 @@ const styles = StyleSheet.create({
   cardContainer: {
     height: 50,
     marginVertical: 30,
+  },
+  backgroundContainer: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    margin: 20,
+  },
+  paypalImageContainer: {
+    alignItems: 'center',
+    marginVertical: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
+  paypalImage: {
+    width: 150,
+    height: 50,
+  },
+  cardManagementTitle: {
+    fontSize: 18,
+    color: 'white',
+    fontFamily: Fonts.Title,
+  },
+  cardImageContainer: {
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  cardImage: {
+    width: 300, // Adjust the width as necessary
+    height: 190, // Adjust the height as necessary
+  },
+  title: {
+    fontSize: 24,
+    color: 'white',
+    fontFamily: Fonts.Title,
+    alignSelf: 'center',
+  },
+  payButtonContainer: {
+    marginTop: 20,
+    padding: 15,
+    backgroundColor: 'red', // or any color you prefer
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5, // for Android
+  },
+  payButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
