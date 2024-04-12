17ec7942378c98525d7ce05dc3363ed64882f987
Rintt <43971333+Rintt@users.noreply.github.com>
2024-04-12 16:14:43 +0200

    Delete node_modules directory

--- node_modules/react-native-view-pdf/ReactNativeViewPDF/RNPDFViewManager.h
+++
@@ -1,9 +0,0 @@
-#import <React/RCTViewManager.h>
-#import <React/RCTConvert.h>
-#import "RNPDFView.h"
-
-@interface RNPDFViewManager : RCTViewManager
-
-@property (nonatomic) RNPDFView *pdfView;
-
-@end
